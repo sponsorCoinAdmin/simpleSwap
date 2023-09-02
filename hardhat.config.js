0f85b2eaf94cb4105f4b4b1dbb9c9d3e1af85a04
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2023-09-02 04:27:54 -0400

    WIP

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,10 +9,25 @@ module.exports = {
       forking: {
         // url: "https://mainnet.infura.io/v3/{YOUR INFURA KEY HERE}"
         // or if using .env file use example similar to below
-        // url: process.env.MAINNET_INFURA_TEST_URL
+        url: process.env.MAINNET_INFURA_TEST_URL
         // url: process.env.GOERLI_INFURA_TEST_URL
-        url: process.env.SEPOLIA_INFURA_TEST_URL
+        // url: process.env.SEPOLIA_INFURA_TEST_URL
       }
     }
+    /*
+    ,
+    mainnet: {
+      url: process.env.MAINNET_ALCHEMY_TEST_URL,
+      accounts: [process.env.WALLET_SECRET]
+    },
+    goerli: {
+      url: process.env.GOERLI_ALCHEMY_TEST_URL,
+      accounts: [process.env.WALLET_SECRET]
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_ALCHEMY_TEST_URL,
+      accounts: [process.env.WALLET_SECRET]
+    }
+    */
   }
 };
